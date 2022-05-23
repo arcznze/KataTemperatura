b53512f2a80c4845618103478aa32c0b110980c6
DESIREE LARISSA MARINEZ JIMENEZ <1101374@est.intec.edu.do>
2022-05-23 14:30:18 -0400

    Substract method added.

--- Kata_Temperatura/Temperature.cs
+++ Kata_Temperatura/Temperature.cs
@@ -109,10 +109,23 @@ namespace Kata_Temperatura
             }
 
         }
-        //public double Substract(double x, string y)
-        //{
-        //    Temperature temperature 
-        //}
+        public double Substract(double x, string y)
+        {
+            Temperature tempo = new Temperature(temperature, scale);
+            if (y == tempo.scale)
+                return tempo.temperature - x;
+            else
+            {
+                if (tempo.scale == "Kelvin" && y != "Kelvin")
+                    return tempo.temperature - ToKelvin(x);
+                else if (tempo.scale == "Celsius" && y != "Celsius")
+                    return tempo.temperature - ToCelsius(x);
+                else if (tempo.scale == "Fahrenheit" && y != "Fahrenheit")
+                    return tempo.temperature - ToFahrenheit(x);
+                else
+                    return 0;
+            }
+        }
 
     }
 }
