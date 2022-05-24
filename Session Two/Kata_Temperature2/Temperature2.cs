2c79d2973b74a221330b1b1be71a6ac1207c971d
DESIREE LARISSA MARINEZ JIMENEZ <1101374@est.intec.edu.do>
2022-05-24 11:25:20 -0400

    Celsius method added.

---
+++ Session Two/Kata_Temperature2/Temperature2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Temperature2
+{
+	public Temperature2()
+	{
+		double temperature;
+		string scale;
+
+		public Temperature2(double temperature, string scale)
+        {
+			this.temperature = temperature;
+			this.scale = scale;
+        }
+
+		public double Celsius(double x)
+        {
+            Temperature2 temp = new Temperature2(temperature, scale);
+            switch (temp.scale)
+            {
+                case temp.scale == "Fahrenheit":
+                    x = (x - 32) / 1.8;
+                    return x;
+                case temp.scale == "Kelvin":
+                    x -= 273.15;
+                    return x;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
